6fea256fc32715bc296cae72282bb59f8d24cfea
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2022-11-23 00:45:47 +0300

    Secret Key returned
    Secret Key returned

--- java_ztm/settings.py
+++ java_ztm/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '396!e7v#mrctw*=a^&%s0-+e&-)7g8*blx&zq=%rulx5f=c0ex'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
